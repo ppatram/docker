87c190cf4b8ccc205ff70bdaefcfbdfcb2408ebb
parm patram <ppatram@gmail.com>
2026-06-23 20:55:36 +0000

    moving all to hands-on branch

--- snapshots.sh
+++ snapshots.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
-set -x
+set -e
 snapname=$(echo $1 | tr ' ' '-')
-ssh root@master "shutdown -h now"
-ssh root@worker1 "shutdown -h now"
-ssh root@worker2 "shutdown -h now"
+message="hello world"
 
-sleep 15
-
-for vm in master worker1 worker2; do
-	virsh shutdown $vm 2>/dev/null || true
-done
-sleep 15
-for vm in master worker1 worker2; do
+for vm in master worker1; do
 	virsh shutdown $vm 2>/dev/null || true
 	virsh snapshot-create-as $vm $snapname $snapname --atomic
 	virsh snapshot-list $vm
-	virsh start $vm 2>/dev/null || true
 done
--- socat.sh
+++ socat.sh
@@ -2,3 +2,5 @@ nohup  socat TCP-LISTEN:9090,fork TCP:master:9090 &
 nohup socat TCP-LISTEN:9100,fork TCP:master:9100 &
 nohup socat TCP-LISTEN:9093,fork TCP:master:9093 &
 nohup socat TCP-LISTEN:9200,fork TCP:node1:9100 &
+nohup socat TCP-LISTEN:9300,fork TCP:node2:9100 &
+nohup socat TCP-LISTEN:3000,fork TCP:master:3000 &
